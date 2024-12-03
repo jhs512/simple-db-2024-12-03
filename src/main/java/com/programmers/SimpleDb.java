e51cff2a5a9e9af505707af09bc2813210b72065
injunKang <149602262+InJunKangW@users.noreply.github.com>
2024-12-03 15:21:32 +0900

    init

---
+++ src/main/java/com/programmers/SimpleDb.java
@@ -0,0 +1,42 @@
+package com.programmers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SimpleDb {
+    private final Connection connection;
+
+//    "localhost", "root", "lldj123414", "simpleDb__test"
+    public SimpleDb(String localhost, String id, String password, String database ) {
+        try{
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:3306/%s", localhost, database), id, password);
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setDevMode(boolean devMode) {
+
+    }
+
+    public Sql genSql(){
+        return new Sql(connection);
+    }
+
+    public void run(Object... sql){
+        String query = String.valueOf(sql[0]);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            if(sql.length > 1){
+                for (int i = 1; i < sql.length; i++) {
+                    preparedStatement.setObject(i, sql[i]);
+                }
+            }
+            preparedStatement.executeUpdate();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/programmers/Sql.java
@@ -0,0 +1,117 @@
+package com.programmers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Sql {
+    private final Connection connection;
+    private final StringBuilder stringBuilder = new StringBuilder();
+    private final List<Object> parameters = new ArrayList<>();
+
+    public Sql(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Sql append(String query){
+        stringBuilder.append(" ").append(query);
+        return this;
+    }
+
+    public Sql append(String query, Object... args){
+        stringBuilder.append(" ").append(query);
+        parameters.addAll(Arrays.asList(args));
+        return this;
+    }
+
+    public long insert(){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString(),
+                PreparedStatement.RETURN_GENERATED_KEYS)){
+            for(int i = 0; i < parameters.size(); i++){
+                preparedStatement.setObject(i + 1, parameters.get(i));
+            }
+            preparedStatement.executeUpdate();
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {  // 커서가 유효한지 확인
+                return generatedKeys.getLong(1);
+            } else {
+                throw new SQLException("No generated key obtained.");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private long modify(){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString())){
+            for(int i = 0; i < parameters.size(); i++){
+                preparedStatement.setObject(i + 1, parameters.get(i));
+            }
+            return preparedStatement.executeUpdate();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long update(){
+       return modify();
+    }
+
+    public long delete(){
+        return modify();
+    }
+
+    public List<Map<String, Object>> selectRows(){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString())){
+            for(int i = 0; i < parameters.size(); i++){
+                preparedStatement.setObject(i + 1, parameters.get(i));
+            }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Map<String, Object>> rows = new ArrayList<>();
+
+            while(resultSet.next()){
+                Map<String, Object> row = new HashMap<>();
+                row.put("id", resultSet.getLong(1));
+                row.put("createdDate", resultSet.getTimestamp(2).toLocalDateTime());
+                row.put("modifiedDate", resultSet.getTimestamp(3).toLocalDateTime());
+                row.put("title", resultSet.getString(4));
+                row.put("body", resultSet.getString(5));
+                row.put("isBlind", resultSet.getBoolean(6));
+                rows.add(row);
+            }
+            return rows;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, Object> selectRow(){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(stringBuilder.toString())){
+            for(int i = 0; i < parameters.size(); i++){
+                preparedStatement.setObject(i + 1, parameters.get(i));
+            }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Map<String, Object> row = new HashMap<>();
+
+            if(resultSet.next()){
+                row.put("id", resultSet.getLong(1));
+                row.put("createdDate", resultSet.getTimestamp(2).toLocalDateTime());
+                row.put("modifiedDate", resultSet.getTimestamp(3).toLocalDateTime());
+                row.put("title", resultSet.getString(4));
+                row.put("body", resultSet.getString(5));
+                row.put("isBlind", resultSet.getBoolean(6));
+            }
+            return row;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
