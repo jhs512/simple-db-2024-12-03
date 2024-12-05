d47de21f96bb7b82e0bd1609248d9b1a35529ba7
jhs512 <jangka512@gmail.com>
2024-12-05 14:46:25 +0900

    appendId, g

--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Sql {
     private final SimpleDb simpleDb;
@@ -24,6 +26,16 @@ public class Sql {
         }
 
         return this;
+    }
+
+    public Sql appendIn(String sqlBit, Object... params) {
+        String inClause = IntStream.range(0, params.length)
+                .mapToObj(i -> "?")
+                .collect(Collectors.joining(", "));
+
+        sqlBit = sqlBit.replace("?", inClause);
+
+        return append(sqlBit, params);
     }
 
     private String toSql() {
